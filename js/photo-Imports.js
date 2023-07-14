7f8db31aa41bb941e8a0257bdae707d475b2a8f4
NovicovMykhailo <mikenovicov@gmail.com>
2023-07-14 16:44:51 +0300

    update json links

---
+++ js/photo-Imports.js
@@ -0,0 +1,180 @@
+// P1
+
+// https://i.ibb.co/YXjF0nk/project-1-450.jpg
+// https://i.ibb.co/86cRzwh/project-1-450.webp
+// https://i.ibb.co/SfgTVjG/project-1-450-2x.jpg
+// https://i.ibb.co/4J56DqZ/project-1-450-2x.webp
+// https://i.ibb.co/sgDhzjn/project-1-354.jpg
+// https://i.ibb.co/tB1CRVf/project-1-354.webp
+// https://i.ibb.co/rcP9sTL/project-1-354-2x.jpg
+// https://i.ibb.co/XDvWpgL/project-1-354-2x.webp
+// https://i.ibb.co/Kr9PT8g/project-1-370.jpg
+// https://i.ibb.co/rHhnk65/project-1-370.webp
+// https://i.ibb.co/wyNB4BZ/project-1-370-2x.jpg
+// https://i.ibb.co/ckLN4v0/project-1-370-2x.webp
+
+// P2
+
+// https://i.ibb.co/qyFPD4C/project-2-450.jpg
+// https://i.ibb.co/HDv6B3G/project-2-450.webp
+// https://i.ibb.co/8KkCppY/project-2-450-2x.jpg
+// https://i.ibb.co/9v4njT3/project-2-450-2x.webp
+
+// https://i.ibb.co/jMzb8gc/project-2-354.jpg
+// https://i.ibb.co/pbn8bPJ/project-2-354.webp
+// https://i.ibb.co/xhqcmzr/project-2-354-2x.jpg
+// https://i.ibb.co/4WfTw8b/project-2-354-2x.webp
+
+// https://i.ibb.co/TqZkP2V/project-2-370.jpg
+// https://i.ibb.co/5B4YJgz/project-2-370.webp
+// https://i.ibb.co/Vjt59hr/project-2-370-2x.jpg
+// https://i.ibb.co/D1f43hP/project-2-370-2x.webp
+
+// P3
+
+// https://i.ibb.co/VTcZ4NP/project-3-450.jpg
+// https://i.ibb.co/bXKqn1V/project-3-450.webp
+// https://i.ibb.co/njGdhwp/project-3-450-2x.jpg
+// https://i.ibb.co/wNMDhHt/project-3-450-2x.webp
+
+// https://i.ibb.co/dp6P0RB/project-3-354.jpg
+// https://i.ibb.co/mRnvyVR/project-3-354.webp
+// https://i.ibb.co/kyKpt5F/project-3-354-2x.jpg
+// https://i.ibb.co/5rSHdMZ/project-3-354-2x.webp
+
+// https://i.ibb.co/f9gyn6s/project-3-370.jpg
+// https://i.ibb.co/vkQGvB5/project-3-370.webp
+// https://i.ibb.co/7xhQ6Xw/project-3-370-2x.jpg
+// https://i.ibb.co/KXyH6LY/project-3-370-2x.webp
+
+// P4
+// https://i.ibb.co/Hgh6KPb/project-4-450.jpg
+// https://i.ibb.co/LJS0YHJ/project-4-450.webp
+// https://i.ibb.co/nzbfpB4/project-4-450-2x.jpg
+// https://i.ibb.co/SRzBFMT/project-4-450-2x.webp
+
+// https://i.ibb.co/MRmL0TB/project-4-354.jpg
+// https://i.ibb.co/P5cDT10/project-4-354.webp
+// https://i.ibb.co/cC23Sqf/project-4-354-2x.jpg
+// https://i.ibb.co/KVBzV0F/project-4-354-2x.webp
+
+// https://i.ibb.co/tq04Z9b/project-4-370.jpg
+// https://i.ibb.co/f11qV0b/project-4-370.webp
+// https://i.ibb.co/FW8PKDf/project-4-370-2x.jpg
+// https://i.ibb.co/yFYVjpN/project-4-370-2x.webp
+
+// P5
+
+// https://i.ibb.co/XCp7zY5/project-5-354.jpg
+// https://i.ibb.co/ZVqBrL8/project-5-354.webp
+// https://i.ibb.co/DDVnbRM/project-5-354-2x.jpg
+// https://i.ibb.co/J71yV5K/project-5-354-2x.webp
+
+// https://i.ibb.co/8BkXFtX/project-5-450.jpg
+// https://i.ibb.co/82y5cFZ/project-5-450.webp
+// https://i.ibb.co/sbhzZnT/project-5-450-2x.jpg
+// https://i.ibb.co/h7CHR0s/project-5-450-2x.webp
+
+
+
+// https://i.ibb.co/MVKGCtg/project-5-370.jpg
+// https://i.ibb.co/2WfCFvx/project-5-370.webp
+// https://i.ibb.co/48zCgTX/project-5-370-2x.jpg
+// https://i.ibb.co/8dK3L0C/project-5-370-2x.webp
+
+// P6
+
+
+// https://i.ibb.co/F0F9mwV/project-6-354.jpg
+// https://i.ibb.co/tB7BnDB/project-6-354.webp
+// https://i.ibb.co/pZnqJ7d/project-6-354-2x.jpg
+// https://i.ibb.co/Lxz4p2V/project-6-354-2x.webp
+
+// https://i.ibb.co/sgWpwwh/project-6-370.jpg
+// https://i.ibb.co/3SS5tzJ/project-6-370.webp
+// https://i.ibb.co/2KzNgjH/project-6-370-2x.jpg
+// https://i.ibb.co/rcSRkqP/project-6-370-2x.webp
+
+// https://i.ibb.co/s9XKh67/project-6-450.jpg
+// https://i.ibb.co/3WYhv4s/project-6-450.webp
+// https://i.ibb.co/1rPd4C5/project-6-450-2x.jpg
+// https://i.ibb.co/n02Zgg5/project-6-450-2x.webp
+
+
+
+// P7
+
+
+// https://i.ibb.co/V9v8SY4/project-7-354.jpg
+// https://i.ibb.co/jhLnJz6/project-7-354.webp
+// https://i.ibb.co/X2mrZqS/project-7-354-2x.jpg
+// https://i.ibb.co/16ZtQFs/project-7-354-2x.webp
+
+// https://i.ibb.co/WfmF6S4/project-7-370.jpg
+// https://i.ibb.co/5Lcdthm/project-7-370.webp
+// https://i.ibb.co/k9Wjkr6/project-7-370-2x.jpg
+// https://i.ibb.co/dkmycpT/project-7-370-2x.webp
+
+// https://i.ibb.co/HTTBvDM/project-7-450.jpg
+// https://i.ibb.co/BjrhHwk/project-7-450.webp
+// https://i.ibb.co/02Tgmr2/project-7-450-2x.jpg
+// https://i.ibb.co/6vccKwZ/project-7-450-2x.webp
+
+
+// P8
+
+
+// https://i.ibb.co/q7ckdvs/project-8-354.jpg
+// https://i.ibb.co/9tXv3Hn/project-8-354.webp
+// https://i.ibb.co/fFDvgzy/project-8-354-2x.jpg
+// https://i.ibb.co/5GHZ5md/project-8-354-2x.webp
+
+// https://i.ibb.co/HPvRSmB/project-8-370.jpg
+// https://i.ibb.co/DfZbFHF/project-8-370.webp
+// https://i.ibb.co/drNgMqh/project-8-370-2x.jpg
+// https://i.ibb.co/f4JRHXN/project-8-370-2x.webp
+
+// https://i.ibb.co/gzBzHCn/project-8-450.jpg
+// https://i.ibb.co/g4GCFLd/project-8-450.webp
+// https://i.ibb.co/202qksv/project-8-450-2x.jpg
+// https://i.ibb.co/mbwSG95/project-8-450-2x.webp
+
+
+// P9
+
+
+// https://i.ibb.co/mJGRgjd/project-9-354.jpg
+// https://i.ibb.co/3rq700Q/project-9-354.webp
+// https://i.ibb.co/FYQ18xn/project-9-354-2x.jpg
+// https://i.ibb.co/f9CYQJD/project-9-354-2x.webp
+
+// https://i.ibb.co/DKh6DD4/project-9-370.jpg
+// https://i.ibb.co/gD0V8rb/project-9-370.webp
+// https://i.ibb.co/191pSWw/project-9-370-2x.jpg
+// https://i.ibb.co/8j5xtjF/project-9-370-2x.webp
+
+// https://i.ibb.co/0sC8ksj/project-9-450.jpg
+// https://i.ibb.co/HHWYnpC/project-9-450.webp
+// https://i.ibb.co/D711nm6/project-9-450-2x.jpg
+// https://i.ibb.co/jVqsRw2/project-9-450-2x.webp
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
